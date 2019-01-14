0d3788f3d47b17fc68f8b4c75c2e15c30dc2edb4
simonburghardt <sb234@hdm-stuttgart.de>
2019-01-14 21:52:55 +0100

    added data + data preprocessing

--- crawler.py
+++ crawler.py
@@ -7,24 +7,41 @@ search_url = "http://transcripts.wikia.com/wiki/Cartman_Gets_an_Anal_Probe"
 result = requests.get(search_url)
 html = result.content.decode("utf-8")
 soup = BeautifulSoup(html, features="html.parser")
-
-print(result.url)
+wikitable = soup.select('table.wikitable')
+# print(wikitable)
 
 def crawl_wikia(link):
     result = requests.get(link)
     html = result.content.decode("utf-8")
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, features="html.parser")
     wikitable = soup.select('table.wikitable')
-    print(wikitable)
 
-    read_table()
+    read_table(wikitable)
 
     return True
 
 
-def read_table(soup):
+def read_table(chunk):
 
     speaker_dict = {}
+    current_speaker = ""
+    current_text = ""
+
+    for line in chunk:
+        if "<th>" in str(line):
+            print(current_speaker)
+            current_speaker = str(line)[3:].strip(":")
+            print(current_speaker)
+
+            if current_speaker not in speaker_dict:
+                speaker_dict[current_speaker] = []
+
+
+        if "<td>" in line:
+            current_text = line.strip("<td>", "</th>")
+            speaker_dict[current_speaker] = speaker_dict[current_speaker].append(current_text)
+
+
 
     return speaker_dict
 
@@ -33,3 +50,7 @@ def dict_to_json():
 
     return True
 
+
+dddd = read_table(wikitable)
+
+print(dddd)
