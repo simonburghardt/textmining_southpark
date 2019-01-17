072f9b0cbd75f03517da34a3823c72ad435799b4
simonburghardt <sb234@hdm-stuttgart.de>
2019-01-17 23:15:32 +0100

    swearword dictionary crawler finished

--- crawler.py
+++ crawler.py
@@ -1,56 +1,84 @@
 import requests
+import re
+import string
+import json
 from bs4 import BeautifulSoup
 
 
-search_url = "http://transcripts.wikia.com/wiki/Cartman_Gets_an_Anal_Probe"
+search_url = "https://www.noswearing.com/dictionary/{}"
 
-result = requests.get(search_url)
-html = result.content.decode("utf-8")
-soup = BeautifulSoup(html, features="html.parser")
-wikitable = soup.select('table.wikitable')
-# print(wikitable)
+# Liefert uns das komplette Alphabet als Liste
+alphabet = list(string.ascii_lowercase)
 
-def crawl_wikia(link):
-    result = requests.get(link)
+
+# HTML Request auf Dicitonary Seite. Liefert fett gedruckte Tags zurück
+def crawl_sweardict(link, buchstabe):
+    result = requests.get(link.format(buchstabe))
     html = result.content.decode("utf-8")
     soup = BeautifulSoup(html, features="html.parser")
-    wikitable = soup.select('table.wikitable')
+    chunk = soup.select('b')
 
-    read_table(wikitable)
+    return chunk
+
+
+# Tokenisiert einen Text und entfernt Satzzeichen
+def tokenize(text):
+    tokens = text.split()
+    tokens = [t.strip(".,;!?").lower() for t in tokens]
+    return tokens
+
+
+def filter_list(chunk):
+
+    swearword_list = []
+
+    for entry in chunk:
+
+        # Filtert HTML Tags raus
+        p = re.compile(r'<.*?>')
+        entry = p.sub('', str(entry))
+
+        # Checkt, ob das Schimpfwort aus mehr als 2 Wörtern besteht und filtert ggf.
+        if len(tokenize(entry)) > 2:
+            continue
+
+        # Checkt, ob Nummern im String sind
+        elif bool(re.search(r'\d', entry)) == True:
+            continue
+
+        # Filtert leere oder Wörter mit 2 Buchstaben raus
+        try:
+            if entry[1]:
+                True
+        except:
+            continue
+
+        else:
+            swearword_list.append(entry)
+
+    return swearword_list
+
+
+# Generiert ein JSON File aus einem Dictionary
+def dict_to_json(parsed_data):
+
+    fh = open('data/' + "cursewords" , mode="w", encoding="utf8")
+    json.dump(parsed_data, fh, indent=4)
 
     return True
 
 
-def read_table(chunk):
+def crawl_all_swear_words():
 
-    speaker_dict = {}
-    current_speaker = ""
-    current_text = ""
+    swearwords_per_buchstabe = {}
 
-    for line in chunk:
-        if "<th>" in str(line):
-            print(current_speaker)
-            current_speaker = str(line)[3:].strip(":")
-            print(current_speaker)
+    # Crawlt für jeden Buchstaben die Swearword-Seite und speichert die generierte Liste in einem
+    # Dictionary ab
+    # Beispiel: { "a" : ["asshole", "asscow"], "b" : ["bitch", "blowjob"] }
+    for buchstabe in alphabet:
+        swearwords_per_buchstabe[buchstabe] = filter_list(crawl_sweardict(search_url, buchstabe))
 
-            if current_speaker not in speaker_dict:
-                speaker_dict[current_speaker] = []
+    return swearwords_per_buchstabe
 
 
-        if "<td>" in line:
-            current_text = line.strip("<td>", "</th>")
-            speaker_dict[current_speaker] = speaker_dict[current_speaker].append(current_text)
-
-
-
-    return speaker_dict
-
-
-def dict_to_json():
-
-    return True
-
-
-dddd = read_table(wikitable)
-
-print(dddd)
+dict_to_json(crawl_all_swear_words())
